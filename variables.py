63d1e29bbebd965e16b23504451f2e240c7f1c03
Bimal <me.bimalpdl1@gmail.com>
2024-08-08 12:55:07 +0545

    added variables.py

---
+++ variables.py
@@ -0,0 +1,60 @@
+# Python allows you to assign values to multiple variables in one line
+x,y,z = "Rohini", "Bimal", "Nirmal"   # x = Rohini, y = Bimal , z = Nirmal
+print(x,y,z)
+
+
+# you can assign the same value to multiple variables in one line
+x = y = z = "Rohini"    # initialized all (x,y,z ) by value "Rohini"
+print(x,y,z)
+# using comma(,) to separate variables in print statement will automatically add a whitespace to separate the variables.
+
+# If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables.
+# This is called unpacking.
+names = ["Rohini", "Paudel", "Didey"]
+x,y,z = names
+# using '+' to concatenate string won't add white space to separate the variables in print() statement and we need to specify white space before/after string manually.
+print("Using '+' to concatenate the string: " + x + y + z)
+names = ["Rohini ", "Paudel ", "Didey "]
+x,y,z = names
+print(x+y+z)
+
+
+# if we try to print integer and string variables using '+' operator, the interpreter will throw an error
+# to print int and str variables comma(,) is used to separate the variable
+x = 15
+y = "Didey"
+# print(x + y)      // this will give an error
+print(x,y)
+
+
+
+# local and global variables in python:
+# we need to use 'global' keyword to access the global variable inside a function (if there is a variable containg same variable name)
+
+name = "Rohini"
+
+def greet():
+    print("Hello,",name)
+
+greet()
+
+
+didi = "caring"
+
+def didey():
+    didi = "supportive"
+    print("Didey is",didi)  # prints 'Didey is supportive' using local variable
+didey()
+print("Didey is", didi)    # prints 'Didey is caring' using global variable
+
+
+# accessing and modifying global variable inside the function
+
+x = "awesome"
+
+def myFunc():
+    global x     # to access global variable
+    x = "fantastic"    # changes the value of global variable 'x' from 'awesome' to 'fantastic'
+
+myFunc()
+print("Bimal is", x)
